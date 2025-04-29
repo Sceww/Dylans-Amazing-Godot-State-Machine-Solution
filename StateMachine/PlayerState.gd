3b6b3043936b8b02ac0f9d8121951c570748abaf
Dylan <dylanjmsguzman@gmail.com>
2025-04-29 15:27:20 -0500

    necessary commit

---
+++ StateMachine/PlayerState.gd
@@ -0,0 +1,18 @@
+class_name PlayerState extends State
+
+enum { IDLE, MOVING, AIR }
+
+func handle_input(event: InputEvent) -> void:
+	pass
+
+func state_process(delta: float) -> void:
+	pass
+
+func state_physics(delta: float) -> void:
+	pass
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
---
+++ StateMachine/State.gd
@@ -0,0 +1,16 @@
+class_name State extends Node
+
+func handle_input(event: InputEvent) -> void:
+	pass
+
+func state_process(delta: float) -> void:
+	pass
+
+func state_physics(delta: float) -> void:
+	pass
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
